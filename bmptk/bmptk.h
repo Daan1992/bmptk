9480fe7c45a8b0f76ce39b619094fc2b1856716f
Wouter van Ooijen <wouter@voti.nl>
2012-09-07 14:45:40 +0200

    makefile.local

--- bmptk/bmptk.h
+++ bmptk/bmptk.h
@@ -2,137 +2,30 @@
 //
 // file : bmptk/bmptk.h
 //
-// LICENSE (zlib license)
+// LICENSE (MIT expat license, copy of license.txt)
 //
 // Copyright (c) 2012 Wouter van Ooijen (wouter@voti.nl)
-//
-// This software is provided 'as-is', without any express or implied warranty.
-// In no event will the authors be held liable for any damages arising from
-// the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-//
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software in a
-// product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-//
-// 2. Altered source versions must be plainly marked as such,
-// and must not be misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source distribution.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE..
 //
 //***************************************************************************
-
-// ==========================================================================
-//
-//! \mainpage
-//!
-//! bmptk is a C++ library for bare-metal use on 32-bit embedded systems. 
-//! Bare-metal refers to the situation that there is only the application. 
-//! The application has full control of the hardware, and is running 
-//! without the support of a separate operating system. 
-//! Operating system functionality can still be present, but must be in the 
-//! form of libraries, which are statically linked into the application. 
-//! bmptk contains a number of such is such a libraries.
-//!
-//! The graphics part of the library provides the interface to a 
-//! few LCD screens, and basic graphics functions of drawing lines, 
-//! circles, boxes, characters, and pictures on a graphic screen. 
-//!
-//! The widget part builds on the graphics part to provide a widget 
-//! toolkit with event processing and objects like button, 
-//! slider, and tab. 
-//!
-//! GraWiLi provides a command-line tool (the Python script inline.py) 
-//! that can be used to translate an image (.bmp or .jpg) or font (.ttf) 
-//! files to C++ code that can be included in an application. 
-//! A few pre-translated images and fonts are provided.
-//!
-//! Using GraWiLi requires an interface to a (presumably LCD) screen, 
-//! and for the widget part, a way to capture events. 
-//! Interfaces are available for Windows (to quickly test an application, 
-//! I use DevCpp), NintendoDS (using ??) 
-//! and the FriendlyARM micro2440 board with 2.5" LCD (using ??).
-//! 
-//! GraWiLi is licensed under a very permissive license. 
-//! Basically the only requirements are that a distribution of 
-//! the (original or modified) GraWiLi sources must use the same 
-//! license, and that you don't blame the author for anything. 
-//! No legal requirements are put on the executable code, or on 
-//! the code that you link with the library.
-//!
-//! \par Class overview
-//! 
-//!    - \ref gwlib::vector    : x,y coordinates 
-//!    - \ref gwlib::color     : 3 * 8 bit RGB color
-//!    - \ref gwlib::event     : postion + type of a GUI event
-//! 
-//! Vector and color are basic classes that implement a 2-dimensional 
-//! integer coordinates, and a 3-byte (+ transparency) RGB color encoding.
-//! An event is something that the user caused at a specific location on the
-//! screen, typically a mouse click or a stylus touch.
-//!
-//!    - \ref gwlib::drawable  : things that can be drawn on a screen
-//!    - \ref gwlib::line      : a line
-//!    - \ref gwlib::rectangle : a rectangle (box)
-//!    - \ref gwlib::circle    : a circle
-//!    - \ref gwlib::photo     : a rectangle of pixels
-//!    - \ref gwlib::inline_rgb_photo : a color image stored in ROM
-//!    - \ref gwlib::inline_bw_photo  : a BW image stored in ROM
-//!    - \ref gwlib::font      : pictures for the set of ASCII characters
-//!    - \ref gwlib::format    : specified how a text is drawn
-//!    - \ref gwlib::text      : an ASCII string and a format
-//!
-//! Drawables are things that can be drawn. A drawable specifies its shape
-//! and size, but not its location (starting point), that is supplied
-//! when it is drawn. A photo is a rectangular block of (readonly) pixels.
-//! Inline_rgb_photo and inline_bw_photo are photo's that are stored in
-//! in-line code (in ROM).
-//! 
-//! The inline.py tool can be used to create such an in-line representation.
-//!
-//!     - \ref gwlib::frame     : something you can read and write by pixel
-//!     - \ref gwlib::subframe  : part of a frame
-//!
-//! A frame is the thing on which you can draw a drawable. 
-//! Frame inhertits from photo, so it is also a drawable, so you can draw
-//! the content of a frame on (a part of) another frame.
-//! A subframe is a rectangular part of another frame, which can be
-//! rotated, mirrored, or enlarged.
-//!    
-//!    - \ref gwlib::widget    : can handle events and can (re)draw itself
-//!    - \ref gwlib::wframe    : frame widget (has border and background)
-//!    - \ref gwlib::wtoplevel : widget that has a frame and an eventloop
-//! 
-//! \par GraWiLi Software License - Version 1.0 - 2011-03-16
-//! 
-//! Permission is hereby granted, free of charge, to any person 
-//! or organization obtaining a copy of the software and accompanying 
-//! documentation covered by this license (the "Software") to use, 
-//! reproduce, display, distribute, execute, and transmit the Software, 
-//! and to prepare derivative works of the Software, and to permit 
-//! third-parties to whom the Software is furnished to do so, all subject 
-//! to the following:
-//! 
-//! The copyright notices in the Software and this entire statement, 
-//! including the above license grant, this restriction and the following 
-//! disclaimer, must be included in all copies of the Software, in whole 
-//! or in part, and all derivative works of the Software, unless such 
-//! copies or derivative works are solely in the form of machine-executable 
-//! object code generated by a source language processor.
-//! 
-//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-//! EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-//! OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND 
-//! NON-INFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR ANYONE 
-//! DISTRIBUTING THE SOFTWARE BE LIABLE FOR ANY DAMAGES OR OTHER LIABILITY, 
-//! WHETHER IN CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
-//! CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
-//! IN THE SOFTWARE.
-//
 
 #pragma once
 #ifndef BMPTK_H
@@ -176,7 +69,7 @@
    namespace bmptk {
    
       //! bmtkp version
-      const char version[] = "V00.01 ( work-in-progress @ 2012-07-19)";   
+      const char version[] = "V01.00 ( work-in-progress @ 2012-09-06)";   
       
         //! time units
         //
@@ -185,8 +78,8 @@
         //!
         //!    500 * bmptk::ms
         //!
-        //! Please do not make your code depend on the exact values, they
-        //! might change in future versions!
+        //! Please do not make your code depend on the exact values, 
+        //! they! might change in future versions!
       enum bmptk_time_units { 
          s  = 1000 * 1000, 
          ms = 1000,
@@ -197,12 +90,14 @@
       void wait( unsigned int t );      
       
    };  
-                 
+           
+   // include the graphics part only if requested to do so              
    #ifdef BMPTK_GRAPHICS
       #include "bmptk_graphics.h"
       #include "bmptk_font_default.h"
    #endif
    
+   // include the target-specific header files
    #ifdef TARGET_nds
       #include "bmptk_nds.h"
    #endif              
---
+++ bmptk/documentation.h
@@ -0,0 +1,33 @@
+//***************************************************************************
+//
+// file : bmptk/documentation.h
+//
+// This file contains Doxygen-format bmptk documentation that is not
+// related to specific source code. It could be in bmptk.h, but that might
+// slow down compilation.
+//
+//***************************************************************************
+
+// ==========================================================================
+//
+//! \mainpage
+//!
+//! bmptk is a C++ library and build tool for bare-metal use on 32-bit 
+//! embedded systems. 
+//! Bare-metal refers to the situation that there is only the application. 
+//! The application has full control of the hardware, and is running 
+//! without the support of a separate operating system. 
+//! Operating system functionality can still be present, but must be in the 
+//! form of libraries, which are statically linked into the application. 
+//
+// ==========================================================================
+
+// ==========================================================================
+//
+//! \PSPad
+//!
+//! PSPad (www.pspad.com) is an editor that can be configured to work
+//! with bmptk. 
+//
+// ==========================================================================
+
