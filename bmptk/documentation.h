07a723df179ecca39526399be36d0aaa3c29d75e
Wouter van Ooijen <wouter@voti.nl>
2012-09-07 15:25:10 +0200

    libbgi.a added

--- bmptk/documentation.h
+++ bmptk/documentation.h
@@ -19,12 +19,43 @@
 //! without the support of a separate operating system. 
 //! Operating system functionality can still be present, but must be in the 
 //! form of libraries, which are statically linked into the application. 
+//!
+//! This version supports the targets 
+//!    - win: uses DevCpp to create a windows executable
+//!    - nds: uses DevKitPro and DeSmuME
+//
+// ==========================================================================
+
+
+
+// ==========================================================================
+//
+//! \page PSPad
+//!
+//! PSPad (www.pspad.com) is an editor that can be configured to work
+//! with bmptk. 
 //
 // ==========================================================================
 
 // ==========================================================================
 //
-//! \PSPad
+//! \page target win
+//!
+//! The target win is used to create a windows executable.
+//! This target provides a quick way to run a bmptk application.
+//! it is not meant to be used to create full-blown windows applications.
+//!
+//! To support this target the DevCpp environment must be installed.
+//! When it is not installed in the default installation directory
+//! (C:\Dev-Cpp) the makefile.local must be edited to reflect the directory
+//! DevCpp is installed in.
+//! 
+//
+// ==========================================================================
+
+// ==========================================================================
+//
+//! \page target nds
 //!
 //! PSPad (www.pspad.com) is an editor that can be configured to work
 //! with bmptk. 
